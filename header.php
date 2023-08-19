4d1a9486cd50c8b9dec97448c41a93be4d6f18d5
yoshikazu-ishitsuka <align.prog.flex.0812@gmail.com>
2023-08-19 20:26:56 +0900

    Update: header.phpの編集

---
+++ header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="Yosu-Portfolio yoshikazu_ishitsukaのポートフォリオサイトです">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php wp_head(); ?>
+</head>
+
+<body class="home" <?php body_class(); ?>>
+<?php wp_body_open(); ?>
+    <header class="l-header">
+        <div class="l-header__inner">
+            
+        </div>
+
+    </header>
+    
+</body>
+</html>
