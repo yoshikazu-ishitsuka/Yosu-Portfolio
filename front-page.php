03ae4b2a74445d4a1a4146591fd7372fd39eab8d
yoshikazu-ishitsuka <align.prog.flex.0812@gmail.com>
2023-08-21 22:20:50 +0900

    Update: プロフィールの途中まで完了

--- front-page.php
+++ front-page.php
@@ -1,17 +1,41 @@
 <?php get_header(); ?>
     <main class="l-main">
-        <article class="p-visual__main">
+        <section class="p-visual__main">
             <div class="p-visual__main__inner">
                 <img class="p-visual__main__img" src="<?php echo esc_url( get_theme_file_uri( '/images/main_visual.png' )); ?>" alt="風景写真">
                 <h2 class="p-visual__main__title">
                     <?php bloginfo( 'description' ); ?>
                 </h2>
             </div>
-            <section class="p-content">
+        </section>
+
+        <article class="p-content">
+
+            <section class="p-news">
 
             </section>
-
+            <section class="p-profile">
+                <dl class="p-profile__inner">
+                    <dt class="c-title__profile c-text__underline c-text__undertext">
+                        プロフィール
+                    </dt>
+                    <dd class="c-text__profile">
+                        <dl>
+                            <dt class="personal">
+                                名前　生年月日
+                            </dt>
+                            <dd class="introduction">
+                                プロフィールが入ります
+                            </dd>
+                        </dl>
+                    </dd>
+                </dl>
+                <div class="p-profile__img">
+                    <img src="<?php echo esc_url( get_theme_file_uri( '/images/profile.png' )); ?>" alt="">
+                </div>
+            </section>
         </article>
+
         
     </main>
 <?php get_footer(); ?>
