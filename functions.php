16354aada638838ff32ea60d9617c1dd7ac3b0c4
yoshikazu-ishitsuka <align.prog.flex.0812@gmail.com>
2023-08-24 21:05:57 +0900

    Update: カスタム投稿タイプ（制作実績）の追加

--- functions.php
+++ functions.php
@@ -37,3 +37,61 @@ function add_additional_class_on_li( $classes, $item, $args )
     return $classes;
 }
 add_filter('nav_menu_css_class', 'add_additional_class_on_li', 1, 3);
+
+// カスタム投稿設定
+add_action('init', 'create_post_type');
+function create_post_type()
+{
+    //投稿時に使用できる投稿用のパーツを指定
+    $supports = array(
+        'title', //タイトルフォーム
+        'editor', //エディター(内容の編集)
+        'thumbnail', //アイキャッチ画像
+        'author', //投稿者
+        'excerpt', //抜粋
+        'revisions', //リビジョンを保存
+    );
+    register_post_type(
+        'works', // 投稿タイプ名
+        [
+        'labels' => [
+            'name' => '制作実績', // 管理画面上で表示する投稿タイプ名
+            'add_new' => '新規追加', // 新規追加のラベル
+            // 'add_new_item' => 'サンプル投稿新規登録', // 編集画面ラベル(新規登録時)
+            // 'edit_item' => 'サンプル投稿編集', //編集画面ラベル(既存投稿編集時)
+            // 'menu_name' => 'サンプル投稿', //管理画面メニュー(親ラベル)
+            'all_items' => '制作実績一覧', //管理画面メニュー(一覧ラベル)
+            // 'search_items' => 'サンプル投稿を検索' , //検索フォームボタンラベル
+            // 'singular_name' => 'サンプル投稿識別名',    // カスタム投稿の識別名
+        ],
+        'public'        => true,  // カスタム投稿タイプの表示(trueにする)
+        'has_archive'   => true, // カスタム投稿一覧(true:表示/false:非表示)
+        'menu_position' => 5,     // 管理画面上での表示位置
+        'show_in_rest'  => true,  // true:「Gutenberg」/ false:「ClassicEditor」
+        'supports' => $supports
+        ]
+    );
+
+    // register_taxonomy(
+    //     'news-cat',
+    //     'news',
+    //     array(
+    //         'label' => 'ニュースカテゴリー',
+    //         'hierarchical' => true, // “true”だと「カテゴリー」のように。”false”だと「タグ」のような振る舞いになります
+    //         'public' => true,
+    //         'show_in_rest' => true,
+    //     )
+    // );
+
+    // register_taxonomy(
+    //     'news-tag',
+    //     'news',
+    //     array(
+    //         'label' => 'ニュースタグ',
+    //         'hierarchical' => false,
+    //         'public' => true,
+    //         'show_in_rest' => true,
+    //         'update_count_callback' => '_update_post_term_count', // カスタムタクソノミーをタグのように使いたい場合は、オプションの'update_count_callback' => '_update_post_term_count'を必ず追加します
+    //     )
+    // );
+}
