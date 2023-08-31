23bd7f28dc58b1413dc4df09f69df0981f200a2d
yoshikazu-ishitsuka <align.prog.flex.0812@gmail.com>
2023-08-31 10:57:22 +0900

    Update: プライバシーポリシーページのスタイリング

---
+++ privacy-policy.php
@@ -0,0 +1,21 @@
+<?php get_header(); ?>
+<?php get_sidebar(); ?>
+
+    <section class="p-privacy-policy">
+        <?php if (have_posts()) : ?>
+            <h1 class="c-title__privacy-policy">
+                <?php
+                    $title = esc_html( get_the_title() );
+                    if ( empty( $title ) ) {
+                        $title = 'タイトルなし';
+                    }
+                    echo $title;
+                ?>
+            </h1>
+            <?php while (have_posts()) : the_post(); ?>
+                <?php the_content(); ?>
+            <?php endwhile; ?>
+        <?php endif; ?>
+    </section>
+
+<?php get_footer(); ?>
