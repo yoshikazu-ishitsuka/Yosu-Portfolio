7beac7851fcb2fa81949a52bdc7d8f44782627fd
yoshikazu-ishitsuka <align.prog.flex.0812@gmail.com>
2023-08-31 11:21:21 +0900

    Update: プライバシーポリシーページの修正

--- privacy-policy.php
+++ privacy-policy.php
@@ -4,13 +4,15 @@
     <section class="p-privacy-policy">
         <?php if (have_posts()) : ?>
             <h1 class="c-title__privacy-policy">
-                <?php
-                    $title = esc_html( get_the_title() );
-                    if ( empty( $title ) ) {
-                        $title = 'タイトルなし';
-                    }
-                    echo $title;
-                ?>
+                <span>
+                    <?php
+                        $title = esc_html( get_the_title() );
+                        if ( empty( $title ) ) {
+                            $title = 'タイトルなし';
+                        }
+                        echo $title;
+                    ?>
+                </span>
             </h1>
             <?php while (have_posts()) : the_post(); ?>
                 <?php the_content(); ?>
