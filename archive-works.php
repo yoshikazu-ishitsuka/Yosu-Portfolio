9aebced7b91f5e50cf2be2659bdb64727e4530bd
yoshikazu-ishitsuka <align.prog.flex.0812@gmail.com>
2023-09-10 16:06:21 +0900

    Update: 制作実績一覧のアーカイブページにシングルページへのリンクを追加

--- archive-works.php
+++ archive-works.php
@@ -6,30 +6,32 @@
         <h2 class="c-title__archive">
             <span>制作実績一覧</span>
         </h2>
-
         <ul class="p-works__archive__list">
             <?php if (have_posts()) : ?>
                 <?php while (have_posts()) : the_post(); ?>
                     <li class="p-works__archive__item" <?php post_class(); ?>>
                         <?php wp_link_pages(); ?>
-
                         <div class="p-introduction__main">
-                            <?php if ( has_post_thumbnail()) : ?>
-                            <?php echo get_the_post_thumbnail('', 'medium_large', array( 'class' => 'p-introduction__image') ); ?>
-                            <?php else : ?>
-                                <img class="p-introduction__image" src="<?php echo esc_url( get_theme_file_uri('/images/top/main_visual.webp') ); ?>" alt="アイキャッチ画像">
-                            <?php endif; ?>
+                            <a href="<?php the_permalink(); ?>">
+                                <span class="c-image__mask">
+                                    <?php if ( has_post_thumbnail()) : ?>
+                                        <?php echo get_the_post_thumbnail('', 'medium_large', array( 'class' => 'p-introduction__image--works') ); ?>
+                                    <?php else : ?>
+                                        <img class="p-introduction__image" src="<?php echo esc_url( get_theme_file_uri('/images/top/main_visual.webp') ); ?>" alt="アイキャッチ画像">
+                                    <?php endif; ?>
+                                </span>
+
+                                <div class="p-introduction__title--works">
+                                    <?php
+                                        $title = esc_html( get_the_title() );
+                                        if ( empty( $title ) ) {
+                                            $title = 'タイトルなし';
+                                        }
+                                        echo $title;
+                                    ?>
+                                </div>
+                            </a>
                         </div>
-            
-                    <div class="p-introduction__title--works">
-                        <?php
-                            $title = esc_html( get_the_title() );
-                            if ( empty( $title ) ) {
-                                $title = 'タイトルなし';
-                            }
-                            echo $title;
-                        ?>
-                    </div>
                         <?php the_content(); ?>
                     </li>
                 <?php endwhile; ?>
@@ -38,8 +40,6 @@
             <?php endif; ?>
         </ul>
     </div>
-
 </section>
-
 
 <?php get_footer(); ?>
