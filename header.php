f6a432b18e869ffb902acb2426375f86125d9063
yoshikazu-ishitsuka <align.prog.flex.0812@gmail.com>
2023-09-13 09:19:41 +0900

    Update: preloadの追記

--- header.php
+++ header.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="description" content="Yosu-Portfolio yoshikazu_ishitsukaのポートフォリオサイトです">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preload" href="main_visual_sp.webp" as="image" media="(max-width: 520px)">
     <?php wp_head(); ?>
 </head>
 
